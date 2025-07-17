6b31f64c1f8bf336660865fa8391181177e5e441
avilleda55 <villedarmzabner@gmail.com>
2025-07-17 09:55:11 -0600

    Add headers

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
-//echo "Hola, el backend responde.";
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
-// Configurar headers generales
-header("Access-Control-Allow-Origin: *"); // ⚠️ OJO: abre para todos, puedes restringir por dominio
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=UTF-8");
 
-// Manejar preflight OPTIONS (CORS)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
